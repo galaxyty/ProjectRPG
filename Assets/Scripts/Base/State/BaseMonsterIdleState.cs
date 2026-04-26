32549b42c4ccb3c9d1f81b858d37b8b7f1d1d5ac
Host <Host>
2026-04-26 21:03:53 +0900

    불필요한 코드 제거

--- Assets/Scripts/Base/State/BaseMonsterIdleState.cs
+++
@@ -1,4 +0,0 @@
-public abstract class BaseMonsterIdleState : IState
-{
-    public abstract void UpdateState();
-}
--- Assets/Scripts/Base/State/BaseMonsterMoveState.cs
+++
@@ -1,7 +0,0 @@
-public abstract class BaseMonsterMoveState : IState
-{
-    // Å¸°Ù.
-    protected PlayerController _target = null;
-
-    public abstract void UpdateState();
-}
